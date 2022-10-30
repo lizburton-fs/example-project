e33791b8d9f846664fe5eecb46aa7cf35996f0a3
Liz Burton <liz.burton147@gmail.com>
2022-10-30 12:18:21 -0400

    Expand comment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-# Build commands for front end
+# Build commands for front end to create the production build
 rm -rf public
 npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
